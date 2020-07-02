62fba8b46e9627b89b18520fb29f6e63b57318e5
Scott <my@example.com>
2020-07-02 18:45:51 -0500

    structual changes to client and server

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import MonsterList from './componenets/MonsterList';
+
+function App() {
+  return (
+    <Fragment>
+      <MonsterList />
+    </Fragment>
+    
+  );
+}
+
+export default App;
---
+++ client/src/components/MonsterList.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MonsterList = () => {
+    return <h1>Monster List</h1>;
+
+};
+
+export default MonsterList;
