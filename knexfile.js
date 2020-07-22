c334e1362eab9cae32af4bbe24d37de63370dc39
Scott Stringer <scott_ts@hotmail.com>
2020-07-21 21:15:08 -0500

    fixing path of database_logic

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'quest_project',
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: '/migrations',
+    },
+    seeds: {
+      directory: '/seeds',
+  },
+  }
+};
